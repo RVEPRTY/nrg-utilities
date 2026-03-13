0ca922652ebf6135f585918b37503cf8d0356c2f
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-13 12:26:12 -0500

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,20 +1,51 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { sendModLog } = require("../../core/modLogs");
 
-module.exports={
+module.exports = {
 
-data:new SlashCommandBuilder()
+data: new SlashCommandBuilder()
 .setName("kick")
 .setDescription("Kick a user")
-.addUserOption(o=>o.setName("user").setRequired(true))
+.addUserOption(option =>
+option
+.setName("user")
+.setDescription("User to kick")
+.setRequired(true)
+)
+.addStringOption(option =>
+option
+.setName("reason")
+.setDescription("Reason for kick")
+.setRequired(false)
+)
 .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
 
 async execute(interaction){
 
+try{
+
 const user = interaction.options.getUser("user");
+const reason = interaction.options.getString("reason") || "No reason provided";
 
-await interaction.guild.members.kick(user);
+const member = await interaction.guild.members.fetch(user.id);
 
-interaction.reply(`${user.tag} kicked`);
+await member.kick(reason);
+
+await interaction.reply(`${user.tag} kicked`);
+
+await sendModLog(interaction.client, interaction.guild, {
+title:"👢 Kick",
+color:"Orange",
+user:user.tag,
+moderator:interaction.user.tag,
+reason
+});
+
+}catch(error){
+
+interaction.reply({content:"Failed to kick user.", ephemeral:true});
+
+}
 
 }
 
