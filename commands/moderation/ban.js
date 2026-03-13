9feebeed291d59b9ca76bc59d9307a4761f98f10
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-13 12:25:43 -0500

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,20 +1,50 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { sendModLog } = require("../../core/modLogs");
 
-module.exports={
+module.exports = {
 
-data:new SlashCommandBuilder()
+data: new SlashCommandBuilder()
 .setName("ban")
 .setDescription("Ban a user")
-.addUserOption(o=>o.setName("user").setRequired(true))
+.addUserOption(option =>
+option
+.setName("user")
+.setDescription("User to ban")
+.setRequired(true)
+)
+.addStringOption(option =>
+option
+.setName("reason")
+.setDescription("Reason for ban")
+.setRequired(false)
+)
 .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
 
 async execute(interaction){
 
+try{
+
 const user = interaction.options.getUser("user");
+const reason = interaction.options.getString("reason") || "No reason provided";
 
-await interaction.guild.members.ban(user);
+await interaction.guild.members.ban(user, { reason });
 
-interaction.reply(`${user.tag} banned`);
+await interaction.reply(`${user.tag} banned`);
+
+await sendModLog(interaction.client, interaction.guild, {
+title:"🔨 Ban",
+color:"Red",
+user:user.tag,
+moderator:interaction.user.tag,
+reason
+});
+
+}catch(error){
+
+console.error(error);
+interaction.reply({content:"Failed to ban user.", ephemeral:true});
+
+}
 
 }
 
