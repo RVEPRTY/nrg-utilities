716a8ce9af3206f46e20b6f7f9285651c0e3ab9f
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-12 22:09:11 -0500

    Update op.js

--- commands/system/op.js
+++ commands/system/op.js
@@ -1,24 +1,36 @@
 const { SlashCommandBuilder } = require("discord.js");
 
-module.exports={
+module.exports = {
 
-data:new SlashCommandBuilder()
+data: new SlashCommandBuilder()
 .setName("op")
 .setDescription("Owner access")
-.addStringOption(o=>o.setName("password").setRequired(true)),
+.addStringOption(option =>
+option
+.setName("password")
+.setDescription("Owner password")
+.setRequired(true)
+),
 
 async execute(interaction){
 
+await interaction.deferReply({ephemeral:true});
+
 const password = interaction.options.getString("password");
 
-if(password !== process.env.OP_PASSWORD)
-return interaction.reply({content:"Wrong password",ephemeral:true});
+if(password !== process.env.OP_PASSWORD){
+return interaction.editReply("Wrong password");
+}
 
 const role = interaction.guild.roles.cache.get(process.env.OWNER_ROLE);
 
+if(!role){
+return interaction.editReply("Owner role not found");
+}
+
 await interaction.member.roles.add(role);
 
-interaction.reply({content:"Owner role granted",ephemeral:true});
+interaction.editReply("Owner role granted");
 
 }
 
