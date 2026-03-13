06c755c54ec0b024b3a5f10ca5ebd00942229609
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-13 11:04:52 -0500

    Create help.js

---
+++ commands/system/help.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+module.exports = {
+
+data: new SlashCommandBuilder()
+.setName("help")
+.setDescription("Open the NRG Utilities help panel"),
+
+async execute(interaction){
+
+const embed = new EmbedBuilder()
+.setTitle("Welcome to the NRG Utilities bot help panel")
+.setDescription("Choose what command category you want to see first.")
+.setColor(0x2b2d31);
+
+const row = new ActionRowBuilder().addComponents(
+
+new ButtonBuilder()
+.setCustomId("help_mod")
+.setLabel("Moderation")
+.setStyle(ButtonStyle.Primary),
+
+new ButtonBuilder()
+.setCustomId("help_links")
+.setLabel("Links")
+.setStyle(ButtonStyle.Success),
+
+new ButtonBuilder()
+.setCustomId("help_utils")
+.setLabel("Utilities")
+.setStyle(ButtonStyle.Secondary),
+
+new ButtonBuilder()
+.setCustomId("help_fun")
+.setLabel("Fun")
+.setStyle(ButtonStyle.Danger)
+
+);
+
+await interaction.reply({
+embeds:[embed],
+components:[row]
+});
+
+}
+
+};
